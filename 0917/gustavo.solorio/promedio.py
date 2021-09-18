1d818297c4c192bfe8a9e9b82c3f55c6e39feff4
Gustavo Solorio <gustavo.solorio@iteso.mx>
2021-09-17 19:14:33 -0500

    Correcion comentarios Promedio Gustavo Solorio

--- 0917/gustavo.solorio/promedio.py
+++ 0917/gustavo.solorio/promedio.py
@@ -5,12 +5,17 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+#PROGRAMA PARA CALCULAR EL PROMEDIO DE 5 NUMEROS INTRODUCIDO POR EL USUARIO
+
+# Se solicitan los 5 numeros uno por uno
 numero1 = eval(input("Ingresa el número 1: "))
 numero2 = eval(input("Ingresa el número 2: "))
 numero3 = eval(input("Ingresa el numero 3: "))
 numero4 = eval(input("Ingresa el numero 4: "))
 numero5 = eval(input("Ingresa el numero 5: "))
+
+# Se calcula el promedio con la suma de los numeros y, como se solicitan 5,
+# se divide al final entre 5
+
 promedio = (numero1 + numero2 + numero3 + numero4 + numero5)/5
 print("el número que ingresaste fue", promedio)
-tipodato = type (promedio)
-print("El tipo de dato es: ",tipodato)
