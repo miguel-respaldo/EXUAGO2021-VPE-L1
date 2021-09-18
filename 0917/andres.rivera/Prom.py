ae868b12bacaea369a7919218f53f54330ba4f6c
Andres Rivera Marquez <andres.rivera@iteso.mx>
2021-09-17 17:02:09 -0700

    Ejercicio Promedio Andres Rivera

---
+++ 0917/andres.rivera/Prom.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# c-basic-offset: 4; tab-width: 8; indent-tabs-mode: nil
+# vi: set shiftwidth=4 tabstop=8 expandtab:
+# :indentSize=4:tabSize=8:noTabs=true:
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+print("Programa que obtiene el promedio de 5 numeros")
+print("Ingrese 5 numeros")
+a = eval(input("Ingrese a: "))
+b = eval(input("Ingrese b: "))
+c = eval(input("Ingrese c: "))
+d = eval(input("Ingrese d: "))
+e = eval(input("Ingrese e: "))
+
+print((a+b+c+d+e)/5)
+
