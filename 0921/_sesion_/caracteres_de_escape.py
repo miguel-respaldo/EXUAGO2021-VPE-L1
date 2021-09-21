b25f60ce42e71af4f4ab96905ba3d28057564f07
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-09-21 08:56:56 -0500

    timer

--- 0921/_sesion_/caracteres_de_escape.py
+++ 0921/_sesion_/caracteres_de_escape.py
@@ -4,7 +4,7 @@
 # :indentSize=4:tabSize=8:noTabs=true:
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
-
+import time
 
 txt = "Esta es una linea\nesta es otra linea"
 print("1: " + txt)
@@ -12,3 +12,9 @@ txt = "Esta es una linea 1234567890abcdefg\resta es otra linea jajaja"
 print("2: " + txt)
 txt = "Esta es una linea\ttabulador\ttab\ttab"
 print("3: " + txt)
+
+
+for i in range(10:-1:-1):
+    print(i,end="")
+    time.sleep(1)
+    print("\r")
