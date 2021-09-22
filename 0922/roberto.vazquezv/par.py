71bc353a69d3cb5c2a8e7a3c0393134ebc78ffa3
Daniela Najar Medrano <daniela.najar@iteso.mx>
2021-09-22 18:25:51 -0500

    numero par o impar

---
+++ 0922/roberto.vazquezv/par.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# c-basic-offset: 4; tab-width: 8; indent-tabs-mode: nil
+# vi: set shiftwidth=4 tabstop=8 expandtab:
+# :indentSize=4:tabSize=8:noTabs=true:
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+#Programa para evaluar si un número es par o es impar
+
+num = eval(input("ingrese valor del número a evaluar: "))
+
+
+if num % 2 == 0: print("Su número es par")
+else: print("Su número es impar")
+
+
+
+
+
+
+
+
+
+
