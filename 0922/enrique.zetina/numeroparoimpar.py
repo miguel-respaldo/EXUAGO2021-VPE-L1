555cf171eac25432b106af5c9204004599eb70e1
Enrique Zetina <enriquekaka@gmail.com>
2021-09-22 18:23:24 -0500

    Trabajo en equipo

---
+++ 0922/enrique.zetina/numeroparoimpar.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# c-basic-offset: 4; tab-width: 8; indent-tabs-mode: nil
+# vi: set shiftwidth=4 tabstop=8 expandtab:
+# :indentSize=4:tabSize=8:noTabs=true:
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+A = eval(input("Ingresa el numero correspondiente a A: "))
+
+if A % 2 != 0:
+    print("el numero es impar")
+else:
+    print("El numero es par")
