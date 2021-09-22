58957f048fe12a61507bb93c389a822a1427de79
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-09-22 17:38:23 -0500

    diferencias de if con if anidado

---
+++ 0922/_sesion_/diferencia_en_if.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# c-basic-offset: 4; tab-width: 8; indent-tabs-mode: nil
+# vi: set shiftwidth=4 tabstop=8 expandtab:
+# :indentSize=4:tabSize=8:noTabs=true:
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+
+x = 50
+
+if x > 10:
+    print("Es mayor a diez,")
+    if x > 20:
+        print("Es mayor a veinte")
+    else:
+        print("Es menor a veinte")
+
+print("-------------")
+
+if x > 10:
+    print("es mayor a diez,".upper())
+elif x > 20:
+    print("Es mayor a veinte".upper())
+else:
+    print("Es menor a veinte".upper())
+
