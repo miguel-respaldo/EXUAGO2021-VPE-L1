bfe6ed385df183c292f97775a7e408474f7deb3c
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-09-28 07:32:09 -0500

    listas

---
+++ 0928/_sesion_/listas1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# c-basic-offset: 4; tab-width: 8; indent-tabs-mode: nil
+# vi: set shiftwidth=4 tabstop=8 expandtab:
+# :indentSize=4:tabSize=8:noTabs=true:
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+lista = ["manzana", "platano", "cereza", "limon", "kiwi", "mango", "melon"]
+
+otra_lista = lista[2:5]
+
+print(otra_lista)
