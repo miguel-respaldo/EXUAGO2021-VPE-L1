4afc643fa24511fa03f802147fa37f1e67b64929
Alejandro Orozco Alonso <alejandro.orozcoa@iteso.mx>
2021-09-28 13:29:11 -0500

    Se agregaron comentarios

--- 0928/alejandro.orozcoa/Matriz.py
+++ 0928/alejandro.orozcoa/Matriz.py
@@ -5,16 +5,22 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or
 
+#Se pide al usuario ingresar tamaño de matriz
 filas = eval(input("Introduce numero de filas: "))
 columnas = eval(input("Introduce numero de columnas: "))
+
+#Se declara una matriz vacia
 matriz = []
 
+#En el primer for se crean las filas 
+#Se ingresan valores en matriz ingresados por el usuario 
 for x in range(filas):
     matriz.append([])
     for y in range(columnas):
         valor = int(input("Ingresa Fila {}, Columna {}: ".format(x,y)))
         matriz[x].append(valor)
 
+#Se imprime la matriz
 for x in matriz:
     print(x)
 
