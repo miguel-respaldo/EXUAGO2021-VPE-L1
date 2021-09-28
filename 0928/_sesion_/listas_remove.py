cc6560a4bf9b78c0659199a00591761af3a2eb01
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-09-28 08:16:03 -0500

    listas remvoe

---
+++ 0928/_sesion_/listas_remove.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# c-basic-offset: 4; tab-width: 8; indent-tabs-mode: nil
+# vi: set shiftwidth=4 tabstop=8 expandtab:
+# :indentSize=4:tabSize=8:noTabs=true:
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+lista = ["manzana", "platano", "cereza", "limon", "kiwi", "mango", "melon"]
+
+print(lista)
+lista.remove("cereza")
+print(lista)
+lista.pop(1)
+print(lista)
+lista.pop()
+print(lista)
+
+
