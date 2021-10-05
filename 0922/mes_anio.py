898337f465d942deeebd1d4ba8af0e1bd3ddd61e
christianefren <89794140+christianefren@users.noreply.github.com>
2021-10-05 01:18:12 -0500

    Add files via upload

---
+++ 0922/mes_anio.py
@@ -0,0 +1,43 @@
+#Christian Efrén
+#Programa que da mes del año en base al número asignado
+
+mes= eval(input("digita mes del año: "))
+
+if mes == 1:
+    print ("el mes de enero")
+
+elif mes == 2:
+    print ("el mes de febrero")
+
+elif mes == 3:
+    print ("el mes de marzo")
+
+elif mes == 4:
+    print ("el mes de abril")
+
+elif mes == 5:
+    print ("el mes de mayo")
+
+elif mes == 6:
+    print ("el mes de junio")
+
+elif mes == 7:
+    print ("el mes de julio")
+
+elif mes == 8:
+    print ("el mes de agosto")
+
+elif mes == 9:
+    print ("el mes de septiembre")
+
+elif mes == 10:
+    print ("el mes de octubre")
+
+elif mes == 11:
+    print ("el mes de noviembre")
+
+elif mes == 12:
+    print ("el mes de noviembre")
+
+else :
+    print("no es un mes del calendario gregoriano")
---
+++ 0922/par_impar.py
@@ -0,0 +1,8 @@
+#Programa para identificar numero par o impar.
+#Christian Efrén
+
+numero = int(input("Ingresa un número: "))
+if numero % 2 == 0:
+    print("El número es par")
+else:
+    print("El número es impar")
