445b2f73991ea398c498d4866d1ee3e7b72937bc
christianefren <89794140+christianefren@users.noreply.github.com>
2021-10-05 01:19:38 -0500

    Delete par_impar.py

--- 0922/par_impar.py
+++
@@ -1,8 +0,0 @@
-#Programa para identificar numero par o impar.
-#Christian Efrén
-
-numero = int(input("Ingresa un número: "))
-if numero % 2 == 0:
-    print("El número es par")
-else:
-    print("El número es impar")
