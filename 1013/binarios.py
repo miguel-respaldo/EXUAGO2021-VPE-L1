f9d16f240a54f5e5ec9099bdce9bad12bcec9977
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-10-13 17:58:40 -0500

    littlel big

--- 1013/binarios.py
+++ 1013/binarios.py
@@ -8,9 +8,12 @@ mis_bytes = bytearray(3)
 
 print(mis_bytes)
 
-mis_bytes[0] = 1
-mis_bytes[1] = 2
-mis_bytes[2] = 3
+mis_bytes[0] = 1 #  0000 0001
+mis_bytes[1] = 2 #  0000 0010
+mis_bytes[2] = 3 #  0000 0011
+
+#         g         8         0
+# 0000 0011 0000 0010 0000 0001
 
 print("-----------")
 print(mis_bytes)
