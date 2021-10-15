00051aa8b549b7d889ff8157c7f372161e3fa483
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-10-15 18:48:02 -0500

    pylint

--- 1015/una_clase.py
+++ 1015/una_clase.py
@@ -4,14 +4,33 @@
 # :indentSize=4:tabSize=8:noTabs=true:
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
+"""
+Este es un modulo de ejemplo
+de clases en python
+"""
 
 class MiClase:
-    prop1 = 4
-    __prop2 = 5
+    """
+    Esta clase es de ejemplo
+    """
+    def __init__(self):
+        self.prop1 = 4
+        self.__prop2 = 5
+
+    def metodos(self):
+        """
+        Este es un ejemplo dumy
+        """
+        return self.prop1 * self.__prop2
+
+    def suma(self):
+        """
+        Este es un ejemplo dumy tambien
+        """
+        return self.prop1 + self.__prop2
 
 
 objeto = MiClase()
 
 print(objeto.prop1)
-print(objeto.__prop2)
-
+### print(objeto.__prop2)
