8f3cf09f6242440674485191955b6c36eeeb2552
Eduardo Almonte <luis.almonte@iteso.mx>
2021-10-18 18:00:47 -0500

    Agregar archivo main.py

---
+++ proyecto/PCerdos/main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# c-basic-offset: 4; tab-width: 8; indent-tabs-mode: nil
+# vi: set shiftwidth=4 tabstop=8 expandtab:
+# :indentSize=4:tabSize=8:noTabs=true:
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+"""
+Ejemplo de un modulo
+"""
+
+import definicion
+import read_file
+
+def main():
+    """
+    Programa principal
+    """
+
+
+
+
+if __name__ == "__main__":
+    main()
+
