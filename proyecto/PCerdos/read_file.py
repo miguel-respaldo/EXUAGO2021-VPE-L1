ef79e60351b3257e66df85725137fef49ba16e27
Eduardo Almonte <luis.almonte@iteso.mx>
2021-10-18 16:34:54 -0500

    Lectura de archivo

---
+++ proyecto/PCerdos/read_file.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# c-basic-offset: 4; tab-width: 8; indent-tabs-mode: nil
+# vi: set shiftwidth=4 tabstop=8 expandtab:
+# :indentSize=4:tabSize=8:noTabs=true:
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+"""
+Ejemplo de un modulo
+"""
+
+def main():
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("Falta archivo", help="Archivo de Entrada")
+    parser.add_argument("-t", "--text", action="store_true", dest="gen_texto",
+            default=False, help="Generar codigo de texto")
+    parser.add_argument("-o", "--output", action="store", dest="nombre_salida",
+            default="salida.o", help="Nombre de archivo salida")
+
+    args = paser.parse_args()
+
+    print(args.archivo)
+    print(args.nombre_de_salida)
+    print(args.gen_texto)
+
+
+if __name__ == "__main__":
+    main()
+
