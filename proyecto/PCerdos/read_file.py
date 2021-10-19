5d989e350061cccc79e9462eef5d2083f4eb87df
Hugo Vladimir Montes Navarro <belico115@gmail.com>
2021-10-18 20:46:31 -0500

    lectura y almacenamiento de los datos del archivo

--- proyecto/PCerdos/read_file.py
+++ proyecto/PCerdos/read_file.py
@@ -8,22 +8,26 @@
 Ejemplo de un modulo
 """
 import argparse
+import sys
+from definicion.py import #
 
 def main():
     
     parser = argparse.ArgumentParser()
-    parser.add_argument("Falta archivo", help="Archivo de Entrada")
+    parser.add_argument("archivo", help="Archivo de Entrada")
     parser.add_argument("-t", "--text", action="store_true", dest="gen_texto",
             default=False, help="Generar codigo de texto")
-    parser.add_argument("-o", "--output", action="store", dest="nombre_salida",
+    parser.add_argument("-o", "--output", action="store", dest="nombre_de_salida",
             default="salida.o", help="Nombre de archivo salida")
 
     args = parser.parse_args()
 
-    print(args.archivo)
-    print(args.nombre_de_salida)
-    print(args.gen_texto)
-
+    ##print(args.archivo)
+    ##print(args.nombre_de_salida)
+    print(args)
+    a = open(args.archivo)
+    b = a.readlines()
+    print (b)
 
 if __name__ == "__main__":
     main()
