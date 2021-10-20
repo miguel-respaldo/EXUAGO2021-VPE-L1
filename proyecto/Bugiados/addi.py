55710dc5b2bf2815401b21aedc89bfdfb5a53b1d
Moises Perez-Ramos <moises.perez@iteso.mx>
2021-10-19 22:59:50 -0500

    Corrections in print implementation, works right to write each row down others, save the file (.txt) and can open when is called

--- proyecto/Bugiados/addi.py
+++ proyecto/Bugiados/addi.py
@@ -12,17 +12,17 @@ Ejemplo de un modulo
 def addi(rt,rs,imm):
     #rt = rt[1:1]
     #if rt == 'x': 
-    rt = rt[1:2]
-    a = decimal_a_binario(int(rt))
+    rs = rs[1:2]
+    a = decimal_a_binario(int(rs))
     if len(a) == 1: 
         a.insert(0,0)
         a.insert(0,0)
     if len(a) == 2: 
         a.insert(0,0)
-    print("0001",a[2],a[1],a[0])
-    arg = "0001"+ str(a[2]) + str(a[1]) + str(a[0])
+    print("0001",a[0],a[1],a[2])
+    arg = "0001" + str(a[0]) + str(a[1]) + str(a[2]) + "\n"
     print(arg)
-    f = open ('salida.txt', 'w')
+    f = open ('salida.txt', 'a')
     f.write(arg)
     f.close()
 
@@ -37,6 +37,7 @@ def decimal_a_binario(num_dec):
         modulos.insert(0,num_dec % 2)
         num_dec //= 2
     return modulos
+
 def convert_m_i(a):
     for i in range(len(a)):
         a[i] = int(a[i])
@@ -48,8 +49,9 @@ def main():
     
     print("Hola Mundo")
     """
-    addi("x5",0,0)
-    #print(decimal_a_binario(2))
+    addi("x5","x3",0)
+    addi("x0","x1",0)
+    print(decimal_a_binario(1))
     
     #print(len(decimal_a_binario(2)))
 if __name__ == "__main__":
