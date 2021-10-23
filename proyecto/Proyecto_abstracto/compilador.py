fb1edd0cf3990139d6aca99805fc984879f28d2e
alba castro <alba.castro@iteso.mx>
2021-10-22 20:30:51 -0500

    Integracion de comentarios a las funciones del programa

--- proyecto/Proyecto_abstracto/compilador.py
+++ proyecto/Proyecto_abstracto/compilador.py
@@ -1,7 +1,14 @@
+"""
+Proyecto final Ensamblador del equipo "Proyecto_abstracto" con Alba Castro y
+Jesus Apodaca.
+Este programa lee como entrada un archivo .txt que contiene las instrucciones de
+nmemonicos y registros a interpretar en el archivo que se genera en lenguaje
+maquina .bin
+"""
 from io import open     
 
 
-
+#Funcion para identificar y abrir el archivo a codificar, con extension .txt
 def leer_archivo():
     ##nombre= eval(input("escriba el nombre del archivo "))
     try:
@@ -9,7 +16,8 @@ def leer_archivo():
     except:
         print ("error 404 not fount")        
     return archivo
-        
+
+#Funcion que realiza el objetivo de programa
 def codifica(archivo):
     mensaje_final=''
    # lineas=0
@@ -22,7 +30,8 @@ def codifica(archivo):
     lineas+=1
     texto= archivo.replace(' ','')
     texto=texto.lower()
-            
+
+#matrices para identificar y escribir  los opcodes
     codigos_operaciones = ['add','addi','and','andi','beq','bne','j','jal','jr','lb','or','sb','sll','srl']
     codigos_binarios=['0000','0001','0010','0011','0100','0101','0110','0111','1010','1011','1100','1101','1110','1111']
                      
@@ -64,35 +73,39 @@ def codifica(archivo):
         mensaje_final+=imm
         mensaje_final+='\n'                                
     return mensaje_final
-            
-    def registros(salida_text):
+
+
+def registros(salida_text):
         register=int(salida_tex,16)
         register=int(bin(imm)[2:])
         register=str(imm)
         return imm           
-                                
-    def rutina_etiqueta (salida_tex ,memory):               
+
+#Funcion para escribir numeros negativos que puedan estar como registros    
+def rutina_etiqueta (salida_tex ,memory):               
         valor_negativo = salida_tex.find('-1')
         if valor_negativo!=0:
            valor_etiqueta ='11111111'
            return valor_etiqueta
-                    
+#Se identifican registros en formato hexadecimal para escribirlos en binario de 16 bits                    
         hexa=salida_text.find('x')
         if hexa==0:
            imm=int(salida_tex,16)
            imm=int(bin(imm)[2:])
            imm=str(imm)
            return imm
-                    
+        #ultimo registro, imm            
         for c in range(lineas):
             if salida_tex==memory[c]:
                salida=int(bin(c)[2:])
                imm=str(salida)
                break
                         
-        return imm              
+        return imm
+
+#Funcion para crear y escribir el archivo a mostrar como implementacion del programa
 def escribe_archivo(mensaje_end):
-    name = input("ingrese el nombre final que quiere que tenga su archivo binario")
+    name = input("Ingrese el nombre final que desea tenga su archivo binario")
     name+=".bin"
     arch = open(name,"w")
     arch.write(mensaje_end)
@@ -103,5 +116,5 @@ mensaje_final1=[]
 archivo1 = leer_archivo()
 mensaje_final1 = codifica(archivo1)
 escribe_archivo(mensaje_final1)
-print("archivo generadoro")
+print("Archivo generado")
             
